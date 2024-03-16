4eae9b34b786db7ae5048f33fe4df5dd8cc50b9e
Gökhan Gündüz <me@gokhangunduz.dev>
2024-03-16 16:29:52 +0300

    refactor(hook and image): :tada: add error handler and minify banner

--- src/hook.ts
+++ src/hook.ts
@@ -11,5 +11,11 @@ export function useKeycloak() {
     keycloak: Keycloak;
   } = useContext(KeycloakContext);
 
+  if (!keycloak) {
+    throw new Error(
+      "KeycloakProvider is not defined or an attempt was made to use a hook outside the defined area. Use the useKeycloak hook in the nested objects of the components/pages where KeycloakProvider is defined. KeycloakProvider is similar to react-context structure. For more information about React-Context: https://react.dev/reference/react/createContext"
+    );
+  }
+
   return keycloak;
 }
